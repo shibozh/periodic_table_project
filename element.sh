6802e28450fe0373557cfefb57a16d54a74c44f1
bruce zhang <shibozha@andrew.cmu.edu>
2024-05-31 02:24:14 +0000

    feat: logic_of_input

--- element.sh
+++ element.sh
@@ -5,5 +5,15 @@ echo Please provide an element as an argument.
 INPUT=$1
 
 # 如果是个数字
-
+if [[ $INPUT =~ ^[0-9]+$ ]]
+then
+  echo It is a number
+# 如果不是数字且长度小于等于2
+elif [[ ${#INPUT} -le 2 ]]
+then
+  echo "Its length is <= 2"
+# 如果不是数字且长度大于2
+else
+  echo "Its length is > 2"
+fi
 
